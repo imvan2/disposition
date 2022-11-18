5f39b1d926d4d29889b871c9676b058a151ba19a
Van <vtu7410@gmail.com>
2022-11-17 16:54:48 -0800

    get all accounts endpoint working

--- fastapi-accounts/queries/accounts.py
+++ fastapi-accounts/queries/accounts.py
@@ -1,6 +1,9 @@
 from pydantic import BaseModel
-from typing import Optional
+from typing import List, Optional, Union
 from queries.pool import pool
+
+class Error(BaseModel):
+    message:str
 
 class AccountIn(BaseModel):
     first_name: str
@@ -8,6 +11,7 @@ class AccountIn(BaseModel):
     username: str
     password: str
     email: str
+
 
 class AccountOut(BaseModel):
     id: int
@@ -17,32 +21,62 @@ class AccountOut(BaseModel):
     password: str
     email: str
 
+
 class AccountRepository:
     def create(self, account: AccountIn) -> AccountOut:
         # create a pool of connections to connect the database (hello is someone there)
         with pool.connection() as conn:
 
-# get a cursor(something to run SQL with), someone picks up the phone
-						# with lets us not use try blocks
-          with conn.cursor() as db:
-            #run our insert statement, give them an order after
+            # get a cursor(something to run SQL with), someone picks up the phone
+            # with lets us not use try blocks
+            with conn.cursor() as db:
+                # run our insert statement, give them an order after
 
-            result = db.execute(
-              """
+                result = db.execute(
+                    """
                 INSERT INTO accounts
                   (first_name, last_name, username, password, email)
                 VALUES
                   (%s, %s, %s, %s, %s)
                 RETURNING id;
               """,
-              [
-                account.first_name,
-                account.last_name,
-                account.username,
-                account.password,
-                account.email
-              ]
+                    [
+                        account.first_name,
+                        account.last_name,
+                        account.username,
+                        account.password,
+                        account.email,
+                    ],
+                )
+                id = result.fetchone()[0]
+                old_data = account.dict()
+                return AccountOut(id=id, **old_data)
+
+    def get_all(self) -> Union[Error, List[AccountOut]]:
+      try:
+        with pool.connection() as conn:
+          with conn.cursor() as db:
+            db.execute(
+              """
+              SELECT id, first_name, last_name, username, password, email
+              FROM accounts
+              ORDER BY id;
+              """
             )
-            id = result.fetchone()[0]
-            old_data = account.dict()
-            return AccountOut(id=id, **old_data)
+            result = []
+            for record in db:
+              print("record", record)
+              account = AccountOut(
+                id = record[0],
+                first_name = record[1],
+                last_name = record[2],
+                username = record[3],
+                password = record[4],
+                email = record[5]
+              )
+              result.append(account)
+            return result
+      except Exception as e:
+        print(e)
+        return {"message": "made you look"}  
+        
