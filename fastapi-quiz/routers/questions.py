cc8bc40821e7fa6ceaa2a426bc9441ab31ebad22
Thinh <thinhmai1027@gmail.com>
2022-11-22 14:43:22 -0800

    thinh crud operations for create question and answer done,functional

--- fastapi-quiz/routers/questions.py
+++ fastapi-quiz/routers/questions.py
@@ -1,5 +1,7 @@
-from fastapi import APIRouter, Depends
-from queries.questions import QuestionIn, QuestionRepo, QuestionOut
+from fastapi import APIRouter, Depends, Response, Request
+from queries.questions import (QuestionIn, QuestionRepo, QuestionOut, Error)
+from typing import List, Optional, Union
+
 router = APIRouter()
 
 @router.post("/questions", response_model = QuestionOut)
@@ -11,3 +13,37 @@ def create_questions(
     print("repo::::",repo)
 
     return repo.create(question)
+
+@router.get("/questions", response_model=Union[List[QuestionOut], Error])
+def get_all(
+    repo: QuestionRepo = Depends(),
+):
+    return repo.get_all()
+
+@router.put("/questions/{id}", response_model=Union[QuestionOut, Error])
+def update_question(
+    id: int,
+    question: QuestionIn,
+    repo: QuestionRepo = Depends(),
+) -> Union[Error, QuestionOut]:
+    return repo.update(id, question)
+
+
+
+@router.delete("/questions/{q_number}}", response_model=bool)
+def delete_question(
+    q_number: int,
+    repo: QuestionRepo = Depends(),
+) -> bool:
+    return repo.delete(q_number)
+
+@router.get("/questions/{q_number}", response_model=Optional[QuestionOut])
+def get_one_question(
+  q_number: int,
+  response: Response,
+  repo: QuestionRepo = Depends(),
+) -> QuestionOut:
+  question = repo.get_one(q_number)
+  if question is None:
+    response.status_code = 404
+  return question
