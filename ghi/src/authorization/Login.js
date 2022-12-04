03784e710730d274be19a20b1e5ba6189db3f558
Lewey Melchor <dlmelchor12@gmail.com>
2022-12-04 22:11:41 +0000

    lewey move spotify token to login page, so we can pre authorize users, to...

--- ghi/src/authorization/Login.js
+++ ghi/src/authorization/Login.js
@@ -3,6 +3,37 @@ import { useToken, useAuthContext, getTokenInternal } from "./useToken";
 import { useNavigate } from "react-router-dom";
 
 function Login() {
+   // From developer dashboard
+    const CLIENT_ID = "5a2a9a022fc549efae7b97b447d43b5c"
+    // must be set in the developer dashboard (source of Under Construction Warning)
+    const REDIRECT_URI = "http://localhost:3000/Login"
+    // authorization endpoint
+    const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+    //requirement
+    const RESPONSE_TYPE = "token"
+
+    // Set and maintain state
+    const [token, setToken] = useState('')
+
+   useEffect(() => {
+        const hash = window.location.hash
+        let token = window.localStorage.getItem("token")
+        // how to get token from url (when we have a hashtag and no token)
+        if (!token && hash) {
+            token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+            // set hash token to an empty string
+            window.location.hash = ""
+            // save token to local storage
+            window.localStorage.setItem("token", token)
+        }
+        setToken(token)
+    }, [])
+
+    const logout = () => {
+      setToken("")
+      window.localStorage.removeItem("token")
+    }
+
   const [, login] = useToken();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -23,6 +54,12 @@ function Login() {
     <>
       <br></br>
       <br></br>
+      <h1>Please login to spotify first</h1>
+      <h2>
+      {!token ?
+        <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login to Spotify</a>
+        : <button className="btn btn-dark" onClick={logout}>Logout</button>}
+        </h2>
       <form onSubmit={handleSubmit}>
         <div className="mb-3">
           <label htmlFor="username" className="form-label">
