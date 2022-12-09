30ba8a1190603ef3b399f0ad856b8119e23d4f64
Merick <szetomerick@gmail.com>
2022-12-09 12:45:52 -0800

    quiz

--- fastapi-quiz/main.py
+++ fastapi-quiz/main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
 from routers import questions, answers
-from fastapi.middleware.cors import CORSMiddleware
-import os
+# from fastapi.middleware.cors import CORSMiddleware
+# import os
 
-# from starlette.middleware import Middleware
-# from starlette.middleware.cors import CORSMiddleware
+from starlette.middleware import Middleware
+from starlette.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+# app = FastAPI()
 
 origins = [
     "https://moodz3.gitlab.io",
@@ -15,26 +15,28 @@ origins = [
     "http://localhost:8001/signup",
     "https://accounts-microservice.onrender.com/token",
     "https://accounts-microservice.onrender.com/api/signup/token",
+    "https://quiz-microservice.onrender.com/answers"
 ]
-# app = FastAPI(middleware=Middleware)
 
-# middleware = [
-#     Middleware(
-#         CORSMiddleware,
-#         allow_origins=['*'],
-#         allow_credentials=True,
-#         allow_methods=['*'],
-#         allow_headers=['*']
-#     )
-# ]
+middleware = [
+    Middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=['*'],
+        allow_headers=['*']
+    )
+]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+app = FastAPI(middleware=Middleware)
+
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 app.include_router(questions.router)
 app.include_router(answers.router)
