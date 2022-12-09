0ac33213d140a90d3d0c3b7deacb2b0c69722c44
Lewey <dlmelchor12@gmail.com>
2022-12-08 18:55:32 -0700

    quiz fix

--- fastapi-quiz/main.py
+++ fastapi-quiz/main.py
@@ -1,36 +1,36 @@
 from fastapi import FastAPI
 from routers import questions, answers
-# from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 import os
 
-from starlette.middleware import Middleware
-from starlette.middleware.cors import CORSMiddleware
+# from starlette.middleware import Middleware
+# from starlette.middleware.cors import CORSMiddleware
 
-# app = FastAPI()
+app = FastAPI()
 
-# origins = [
-#     os.environ.get("CORS_HOST", "http://localhost"),
-#     "http://localhost:3000",
-# ]
-app = FastAPI(middleware=Middleware)
-
-middleware = [
-    Middleware(
-        CORSMiddleware,
-        allow_origins=['*'],
-        allow_credentials=True,
-        allow_methods=['*'],
-        allow_headers=['*']
-    )
+origins = [
+    os.environ.get("CORS_HOST", "http://localhost"),
+    "http://localhost:3000",
 ]
+# app = FastAPI(middleware=Middleware)
 
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=origins,
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
+# middleware = [
+#     Middleware(
+#         CORSMiddleware,
+#         allow_origins=['*'],
+#         allow_credentials=True,
+#         allow_methods=['*'],
+#         allow_headers=['*']
+#     )
+# ]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(questions.router)
 app.include_router(answers.router)
