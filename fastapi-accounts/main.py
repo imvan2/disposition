b28b9341962c757b03a0f15f97eee6032b9ac43f
Lewey <dlmelchor12@gmail.com>
2022-12-08 18:22:42 -0700

    fixing new cors error, fix redirect

--- fastapi-accounts/main.py
+++ fastapi-accounts/main.py
@@ -3,26 +3,38 @@ from routers import accounts
 from auth.authenticator import authenticator
 import os
 # from fastapi.middleware.cors import CORSMiddleware
+
+from starlette.middleware import Middleware
 from starlette.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+# app = FastAPI()
 
-origins = [
-    os.environ.get("CORS_HOST", "http://localhost"),
-    "http://localhost:3000",
-    "https://moodz3.gitlab.io",
-    "https://accounts-microservice.onrender.com/token",
-    "https://accounts-microservice.onrender.com/api/signup/token",
+# origins = [
+#     os.environ.get("CORS_HOST", "http://localhost"),
+#     "http://localhost:3000",
+#     "https://moodz3.gitlab.io",
+#     "https://accounts-microservice.onrender.com/token",
+#     "https://accounts-microservice.onrender.com/api/signup/token",
 
+# ]
+app = FastAPI(middleware=middleware)
+
+middleware = [
+    Middleware(
+        CORSMiddleware,
+        allow_origins=['*'],
+        allow_credentials=True,
+        allow_methods=['*'],
+        allow_headers=['*']
+    )
 ]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 app.include_router(accounts.router)
 app.include_router(authenticator.router)
--- fastapi-quiz/main.py
+++ fastapi-quiz/main.py
@@ -3,21 +3,34 @@ from routers import questions, answers
 from fastapi.middleware.cors import CORSMiddleware
 import os
 
+from starlette.middleware import Middleware
+from starlette.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+# app = FastAPI()
 
-origins = [
-    os.environ.get("CORS_HOST", "http://localhost"),
-    "http://localhost:3000",
+# origins = [
+#     os.environ.get("CORS_HOST", "http://localhost"),
+#     "http://localhost:3000",
+# ]
+app = FastAPI(middleware=middleware)
+
+middleware = [
+    Middleware(
+        CORSMiddleware,
+        allow_origins=['*'],
+        allow_credentials=True,
+        allow_methods=['*'],
+        allow_headers=['*']
+    )
 ]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 app.include_router(questions.router)
 app.include_router(answers.router)
